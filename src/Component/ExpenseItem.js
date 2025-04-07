74c0d50eed34922c81956d9a3648a04dfa3338d0
kalkidan12-k <kkflemariam@gmail.com>
2025-04-07 10:39:48 -0700

    create home page

---
+++ src/Component/ExpenseItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function ExpenseItem() {
+  return (
+    <div>
+      <div>March 29</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <p>$250</p>
+      </div>
+    </div>
+  )
+}
