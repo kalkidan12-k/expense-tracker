4daa3b184203021955aa4f1a1e8897ce8c47e072
kalkidan12-k <kkflemariam@gmail.com>
2025-04-09 22:34:59 -0700

    k

--- src/Component/ExpenseItem.js
+++ src/Component/ExpenseItem.js
@@ -6,9 +6,9 @@ export default function ExpenseItem(props) {
     
   return (
     <div className='expense-item'>
-      <div className='expense-item__description'>
+      
         <ExpenseDate/>
-      </div>
+      
       <div>
         <h2>{props.title}</h2>
         <div className='expense-item__price'> ${props.amount}</div>
